004be8304640e47370b1cb14d90040c56316d488
LHwang01 <LawrenceHwang01@gmail.com>
2021-12-05 11:41:49 -0500

    Readded Heroku Proxy

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ window.addEventListener('load', () => {
             longitude = position.coords.longitude;  
             latitude = position.coords.latitude;
 
-            const api = `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=e70cf8b646e845e2a292cee7b31afc11&units=imperial`;
+            const proxy = "https://cors-anywhere.herokuapp.com/";
+            const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=e70cf8b646e845e2a292cee7b31afc11&units=imperial`;
             
             fetch(api)
                 .then(response => {
