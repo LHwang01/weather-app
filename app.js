4e41d98e1f5610b5dc96cd48d730a9dc8d59f8db
LHwang01 <LawrenceHwang01@gmail.com>
2021-12-05 00:59:43 -0500

    Added Icon Feature

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ window.addEventListener('load', () => {
     let temperatureDescription = document.querySelector('.temperature-description');
     let temperatureDegree = document.querySelector(".temperature-degree");    
     let locationTimezone = document.querySelector('.location-timezone');
-
-    console.log("Hi");
+    let icon = document.querySelector('.icon');
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
@@ -13,19 +12,28 @@ window.addEventListener('load', () => {
             latitude = position.coords.latitude;
 
             const proxy = "https://cors-anywhere.herokuapp.com/";
-            const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=e70cf8b646e845e2a292cee7b31afc11&units=imperial `;
+            const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=e70cf8b646e845e2a292cee7b31afc11&units=imperial`;
             
             fetch(api)
                 .then(response => {
                     return response.json();
                 })
                 .then(data => {
+                    console.log(data);
+                    baseIconUrl = "http://openweathermap.org/img/wn/"
+                    finalIconUrl = baseIconUrl + data.weather[0].icon + "@2x.png";
+
                     temperatureDegree.textContent = data.main.temp;
                     temperatureDescription.textContent = data.weather[0].main;
                     locationTimezone.textContent = data.name;
+                    document.getElementById("icon").src = finalIconUrl;
             })
         })
     } else {
         h1.textContent = "Please enable location."
+    }
+
+    function setIcons(icon, iconID) {
+        
     }
 });
