eb315a02b132a5cd28c2f9adef61fa5ab5607d9f
LHwang01 <LawrenceHwang01@gmail.com>
2021-12-06 09:16:18 -0500

    Removed Proxy

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ window.addEventListener('load', () => {
             longitude = position.coords.longitude;  
             latitude = position.coords.latitude;
 
-            const proxy = "https://cors-anywhere.herokuapp.com/";
-            const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=e70cf8b646e845e2a292cee7b31afc11&units=imperial`;
+            const api = `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=e70cf8b646e845e2a292cee7b31afc11&units=imperial`;
                 
             fetch(api)
                 .then(response => {
